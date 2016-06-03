c3baf3aa5e03903cbdd9f29bf869796e0a45ee5f
Simon Scholz <simon.scholz@vogella.com>
2016-06-03 05:34:24 +0200

    [Spring] Make use of rest.js and react
    Change-Id: Icfe7df27d852a4c01f5a742b795f547471ca6546
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.spring.data.rest/src/main/resources/static/app.js
+++ com.vogella.spring.data.rest/src/main/resources/static/app.js
@@ -1,6 +1,59 @@
+'use strict';
+
 const React = require('react');
-const client = require('./client');
+const ReactDOM = require('react-dom');
+const client = require('./restclient');
 
-var arrNo = [1,2,3].map(n => n + 1);
+class App extends React.Component {
 
-document.write(arrNo);
+	constructor(props) {
+		super(props);
+		this.state = {tasks: []};
+	}
+
+	componentDidMount() {
+		client({method: 'GET', path: '/api/tasks'}).done(response => {
+			this.setState({tasks: response.entity._embedded.tasks});
+		});
+	}
+
+	render() {
+		return (
+			<TaskList tasks={this.state.tasks}/>
+		)
+	}
+}
+
+class TaskList extends React.Component{
+	render() {
+		var tasks = this.props.tasks.map(task =>
+			<Task key={task._links.self.href} task={task}/>
+		);
+		return (
+			<table>
+				<tr>
+					<th>Summary</th>
+					<th>Description</th>
+				</tr>
+				{tasks}
+			</table>
+		)
+	}
+}
+
+class Task extends React.Component{
+	render() {
+		return (
+			<tr>
+				<td>{this.props.task.summary}</td>
+				<td>{this.props.task.description}</td>
+			</tr>
+		)
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('content')
+)
+
