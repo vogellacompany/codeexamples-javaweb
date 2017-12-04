578a74951c3dcaae8b54aa6a01853bf47d458081
Jonas Hungershausen <jonas@hungershausen.de>
2017-12-04 20:41:02 +0100

    Added @JsonIgnore annotation
    Signed-off-by: Jonas Hungershausen <jonas@hungershausen.de>

--- com.vogella.issuereport/src/main/java/com/vogella/issuereport/IssueReport.java
+++ com.vogella.issuereport/src/main/java/com/vogella/issuereport/IssueReport.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Data;
 
 @Data
@@ -20,10 +22,13 @@ public class IssueReport {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 	
+	@JsonIgnore
 	private String email;
 	private String url;
 	private String description;
+	@JsonIgnore
 	private boolean markedAsPrivate;
+	@JsonIgnore
 	private boolean updates;
 	private boolean done;
 	private Date created;
@@ -31,6 +36,13 @@ public class IssueReport {
 	
 	public IssueReport() {}
 	
+	public IssueReport(String email, String url, String desc, boolean markedAsPrivate, boolean updates) {
+		this.email = email;
+		this.url = url;
+		this.description = desc;
+		this.markedAsPrivate = markedAsPrivate;
+		this.updates = updates;
+	}
 	
 	@PrePersist
 	void created() {
