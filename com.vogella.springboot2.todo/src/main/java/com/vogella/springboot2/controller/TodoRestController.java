5c5ff9ef9046ea539092660f300014de14ca33f3
Fabian Pfaff <459631+fap-@users.noreply.github.com>
2018-11-01 14:53:42 +0100

    RESTify TodoRestController
    Reworks the api endpoints of the TodoRestController to conform more
    with REST style.

--- com.vogella.springboot2.todo/src/main/java/com/vogella/springboot2/controller/TodoRestController.java
+++ com.vogella.springboot2.todo/src/main/java/com/vogella/springboot2/controller/TodoRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/todo")
 class TodoRestController {
 
 	private TodoService todoService;
@@ -23,27 +25,27 @@ class TodoRestController {
 		this.todoService = todoService;
 	}
 
-	@GetMapping("/getTodos")
+	@GetMapping
 	public Flux<Todo> getTodos(@RequestParam(name = "limit", required = false, defaultValue = "-1") long limit) {
 		return todoService.getTodos(limit);
 	}
 
-	@GetMapping("/getTodoById")
+	@GetMapping("/search")
 	public Mono<Todo> getTodoById(long id) {
 		return todoService.getTodoById(id);
 	}
 
-	@GetMapping("/getBySummary")
+	@GetMapping("/search")
 	public Flux<Todo> getBySummary(String summary) {
 		return todoService.getBySummary(summary);
 	}
 
-	@PostMapping("/newTodo")
+	@PostMapping
 	public Mono<Todo> newTodo(@RequestBody Todo todo) {
 		return todoService.newTodo(todo);
 	}
 
-	@DeleteMapping("/deleteTodo/{id}")
+	@DeleteMapping("/{id}")
 	public Mono<Void> deleteTodo(@PathVariable("id") int id) {
 		return todoService.deleteTodo(id);
 	}
