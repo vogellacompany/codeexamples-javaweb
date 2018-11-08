f5addeef08682cd0e56b6c1df591952dd4e3e409
Simon Scholz <simon.scholz@vogella.com>
2018-11-08 23:51:03 +0100

    Make use of eureka load balancing with the gateway
    Change-Id: I3022f2a64d8e4c1f02e8138e0a1acb7bd70d0c45
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.spring.gateway/src/main/java/com/vogella/spring/gateway/RouteConfig.java
+++ com.vogella.spring.gateway/src/main/java/com/vogella/spring/gateway/RouteConfig.java
@@ -1,18 +1,25 @@
 package com.vogella.spring.gateway;
 
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDiscoveryClient
 public class RouteConfig {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("users", r ->
-					r.path("/user/**")
-				.uri("http://localhost:8081"))
-				.build();
+		return builder.routes().route("users", r -> r.path("/user/**").uri("lb://user")).build();
+	}
+
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient,
+			DiscoveryLocatorProperties properties) {
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 }
