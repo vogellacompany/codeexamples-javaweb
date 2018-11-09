47e02aed7116fb12827e78cba8c9b7bede38948d
Simon Scholz <simon.scholz@vogella.com>
2018-11-09 14:13:42 +0100

    Removed unused DiscoveryClientRouteDefinitionLocator bean
    Change-Id: Ic84af2583da536400dad06da9e294fc60f6cbb2e
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.spring.gateway/src/main/java/com/vogella/spring/gateway/RouteConfig.java
+++ com.vogella.spring.gateway/src/main/java/com/vogella/spring/gateway/RouteConfig.java
@@ -1,9 +1,6 @@
 package com.vogella.spring.gateway;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +12,5 @@ public class RouteConfig {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes().route("users", r -> r.path("/user/**").uri("lb://user")).build();
-	}
-
-	@Bean
-	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient,
-			DiscoveryLocatorProperties properties) {
-		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 }
