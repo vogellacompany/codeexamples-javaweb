aaa28cfa1013cd106ecc37c44547548a4800e1ce
Simon Scholz <simon.scholz@vogella.com>
2018-11-13 13:23:14 +0100

    Change response type of hystrix fallback
    Change-Id: I982718f5f3a9299ae7445687a78454449d63e11e
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.spring.gateway/src/main/java/com/vogella/spring/gateway/HystrixFallbackContoller.java
+++ com.vogella.spring.gateway/src/main/java/com/vogella/spring/gateway/HystrixFallbackContoller.java
@@ -1,5 +1,7 @@
 package com.vogella.spring.gateway;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,7 @@ import reactor.core.publisher.Mono;
 class HystrixFallbackContoller {
 
 	@GetMapping("/fallback")
-	public Mono<String> userFallback() {
-		return Mono.just("Please tell the admin to start a user service");
+	public Mono<ResponseEntity<String>> userFallback() {
+		return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build());
 	}
 }
