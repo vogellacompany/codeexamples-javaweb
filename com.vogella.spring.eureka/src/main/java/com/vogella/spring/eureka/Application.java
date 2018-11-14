f25bdc0b1159104322810fdaae5b743b4752ff27
Simon Scholz <simon.scholz@vogella.com>
2018-11-14 22:25:41 +0100

    Added eureka server annotation
    Change-Id: I80d806859053fa9114314ef52a62ae59ceb3f98d
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.spring.eureka/src/main/java/com/vogella/spring/eureka/Application.java
+++ com.vogella.spring.eureka/src/main/java/com/vogella/spring/eureka/Application.java
@@ -2,8 +2,10 @@ package com.vogella.spring.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
