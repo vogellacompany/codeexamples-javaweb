6656e4e0dc2e96039827dc32c83074919aa67152
Simon Scholz <simon.scholz@vogella.com>
2018-11-16 00:21:48 +0100

    Avoid field injection and use interface instead of impls

--- com.vogella.spring.user/src/main/java/com/vogella/spring/user/security/SecurityConfig.java
+++ com.vogella.spring.user/src/main/java/com/vogella/spring/user/security/SecurityConfig.java
@@ -1,24 +1,19 @@
 package com.vogella.spring.user.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
-
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	@Autowired
-	private SecurityContextRepository securityContextRepository;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -27,7 +22,9 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http) {
+	public SecurityWebFilterChain securitygWebFilterChain(ServerHttpSecurity http,
+			ReactiveAuthenticationManager authenticationManager,
+			ServerSecurityContextRepository securityContextRepository) {
 		return http
 			.csrf()
 			.disable()
